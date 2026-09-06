271c830d6d61fb50ce75ab0e62725672de072fde
hemraj11 <hemrajojha1122334455@gmail.com>
2026-09-06 15:49:52 +0545

    product_details page added

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,5 +12,9 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+        
     def __str__(self):
         return self.category_name
